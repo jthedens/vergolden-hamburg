0d1d74cae5f34c0ae187bc00bc3cbaeb90f3d339
Josefine Theden-Schow <josi.theden@gmx.de>
2020-04-01 11:10:39 +0200

    navbar header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_class?(test_path)
+    if request.path == test_path
+      return 'tab-underlined active'
+    else
+      return 'tab-underlined'
+    end
+  end
 end
